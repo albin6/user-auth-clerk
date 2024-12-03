87e3f09752bb5ce9018f43c43c624d075f130c36
Albin Aji <albinpariyarathu@gmail.com>
2024-12-03 23:22:56 +0530

    backend started

---
+++ server/dist/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.send("Hello, Typescript with Express");
+});
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
